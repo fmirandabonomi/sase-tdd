4980160d42849d44ac07ab737032ba3b51b3c2d4
Fernando Alberto Miranda Bonomi <fmirandabonomi@herrera.unt.edu.ar>
2024-08-05 17:42:34 -0300

    + Avanza frontera minuto

--- src/reloj.c
+++ src/reloj.c
@@ -20,6 +20,7 @@ void Reloj_getTiempo(TiempoBcd *destino)
     for (int i=0;i<TiempoBcd_NUM_DIGITOS;++i) (*destino)[i]=self->tiempo[i];
 }
 
+
 void Reloj_tick(void)
 {
     if (self->tiempo[UNIDAD_SEGUNDO] < 9){
@@ -27,6 +28,10 @@ void Reloj_tick(void)
     }else{
         self->tiempo[UNIDAD_SEGUNDO] = 0;
         self->tiempo[DECENA_SEGUNDO]++;
+        if (self->tiempo[DECENA_SEGUNDO] == 6){
+            self->tiempo[DECENA_SEGUNDO] = 0;
+            self->tiempo[UNIDAD_MINUTO]++;
+        }
     }
 }
 
--- test/test_reloj.c
+++ test/test_reloj.c
@@ -36,6 +36,7 @@ void test_puedePonerseEnHora(void)
     TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE(tiempoSet,tiempo,TiempoBcd_NUM_DIGITOS,"El tiempo establecido debe reflejarse en el reloj");
 }
 
+
 void test_avanzaUnSegundo(void)
 {
     static const TiempoBcd tiempoInicial = {1,3,0,0,3,4};
@@ -50,15 +51,27 @@ void test_avanzaUnSegundo(void)
     TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE(tiempoFinal,tiempo,TiempoBcd_NUM_DIGITOS,"Debe avanzar");
 }
 
-void test_avanzaFronteraDecenaDeSegundo(void)
+void test_avanzaFronteras(void)
 {
-    static const TiempoBcd tiempoInicial = {1,3,0,0,3,9};
-    static const TiempoBcd tiempoFinal = {1,3,0,0,4,0};
+    static const struct Caso {
+        TiempoBcd inicial; 
+        TiempoBcd final;
+        const char *mensaje;
+    } tablaCasos[]={
+        {.inicial = {1,3,0,0,3,9}, .final = {1,3,0,0,4,0}, .mensaje = "A 13:00:39 le sigue 13:00:40"},
+        {.inicial = {1,3,0,0,5,9}, .final = {1,3,0,1,0,0}, .mensaje = "A 13:00:59 le sigue 13:01:00"},
+    };
+    const size_t numCasos = sizeof(tablaCasos)/sizeof(*tablaCasos);
+    enum{TICKS_POR_SEGUNDO = 1};
     TiempoBcd tiempo;
-    unsigned ticksPorSegundo = 1;
-    Reloj_init(ticksPorSegundo);
-    Reloj_setTiempo(&tiempoInicial);
-    Reloj_tick();
-    Reloj_getTiempo(&tiempo);
-    TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE(tiempoFinal,tiempo,TiempoBcd_NUM_DIGITOS,"A las 9 unidades le sigue una decena");
+
+    Reloj_init(TICKS_POR_SEGUNDO);
+
+    for(size_t i=0;i<numCasos;++i){
+        const struct Caso *const caso = tablaCasos + i;
+        Reloj_setTiempo(&caso->inicial);
+        Reloj_tick();
+        Reloj_getTiempo(&tiempo);
+        TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE(caso->final,tiempo,TiempoBcd_NUM_DIGITOS,caso->mensaje);
+    }
 }
