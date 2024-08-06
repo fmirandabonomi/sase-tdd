6847f3dc11d0e6222db0be713c6e6d03356d1144
Fernando Alberto Miranda Bonomi <fmirandabonomi@herrera.unt.edu.ar>
2024-08-06 15:29:34 -0300

    + Contempla avance de tiempo antes de alarma pospuesta

--- test/test_reloj.c
+++ test/test_reloj.c
@@ -172,18 +172,22 @@ void test_puedePosponerLaAlarma(void)
 
 void test_puedePosponerAlSiguienteDia(void)
 {
-    static const TiempoBcd tiempoInicial = {2,3,4,4,0,0};
+    static const TiempoBcd tiempoInicial = {2,3,4,3,5,9};
     static const TiempoBcd tiempoAlarma = {2,3,4,4,0,1};
     static const unsigned minutosEspera = 10*60;
-    static const TiempoBcd tiempo2 = {0,9,4,4,0,0};
+    static const TiempoBcd tiempo2 = {0,9,4,3,5,9};
 
     Reloj_setTiempo(&tiempoInicial);
     Reloj_setTiempoAlarma(&tiempoAlarma);
+    avanzaUnSegundo();
+    TEST_ASSERT_EQUAL_UINT(0,AccionAlarma_vecesDisparada(testigoAlarma));
     avanzaUnSegundo();
 
     Reloj_posponAlarma(minutosEspera);
 
     Reloj_setTiempo(&tiempo2);
+    avanzaUnSegundo();    
+    TEST_ASSERT_EQUAL_UINT(1,AccionAlarma_vecesDisparada(testigoAlarma));
     avanzaUnSegundo();    
     TEST_ASSERT_EQUAL_UINT_MESSAGE(2,AccionAlarma_vecesDisparada(testigoAlarma),"Debe disparar la alarma pospuesta");
 }
